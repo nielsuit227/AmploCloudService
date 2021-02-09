d1cbbda0cebedeee439f215f8f3d4c58195a5d23
Niels Uitterdijk <niels.uit@gmail.com>
2021-02-09 07:47:52 +0100

    CORS, CSRF

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -2,31 +2,33 @@ import os
 from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v4%!pmh6=a%5_ok6!24fllmd-g^7xjy+-6!95ll-r$t63npcxs'
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'amplodb',
-        'USER': 'postgres',
-        'PASSWORD': 'rYs@!r7yAne(@^85',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
 PROJECT_DIR = Path(__file__).parent
-MEDIA_ROOT = os.path.join(BASE_DIR, 'files/')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-CORS_ORIGIN_WHITELIST = ('http://nginx:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:8000')
+
+ALLOWED_HOSTS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1'
+]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_EXPOSE_HEADERS = (
-    'Access-Control-Allow-Origin: http://127.0.0.1:3000',
-)
+
+CSRF_USE_SESSIONS = False
+CSRF_COOKIE_HTTPONLY = False         # Not accessible by client (not important)A
+CSRF_COOKIE_AGE = 8 * 3600           # Expires after 8 hr
+CSRF_COOKIE_SECURE = False           # Only HTTPS
+
+SESSION_COOKIE_HTTPONLY = True      # Not accessible by client 
+SESSION_COOKIE_AGE = 8 * 3600        # Expires after 8 hr
+SESSION_COOKIE_SECURE = False        # Only HTTPS
+
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,7 +46,30 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
 ]
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amplodb',
+        'USER': 'postgres',
+        'PASSWORD': 'rYs@!r7yAne(@^85',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/hour',
+        'user': '25/s',
+    },    
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.SessionAuthentication',
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+}
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +80,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+ROOT_URLCONF = 'core.urls'
+WDGI_APPLICATION = 'core.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -70,8 +97,6 @@ TEMPLATES = [
         },
     },
 ]
-ROOT_URLCONF = 'core.urls'
-WSGI_APPLICATION = 'core.wsgi.application'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -90,4 +115,4 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
